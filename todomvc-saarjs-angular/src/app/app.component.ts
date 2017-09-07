1b9c5bd14b71d9d719826ccedf213b44cb39128b
Thomas Darimont <thomas.darimont@gmail.com>
2017-09-07 22:52:48 +0200

    Add complete angular example

--- todomvc-saarjs-angular/src/app/app.component.ts
+++ todomvc-saarjs-angular/src/app/app.component.ts
@@ -5,26 +5,20 @@ import { Component } from '@angular/core';
   template: `
     <header class="header">
     <h1>
-      Welcome to Title!
+      Welcome to {{title}}
     </h1>
     </header>
     <section class="main" style="background-color: white;">
-      <input class="new-todo" type="text" placeholder="What needs to be done?">
+      <input class="new-todo" type="text" placeholder="What needs to be done?"
+        (keyup.enter)="addTodo($event)">
       <ul class="todo-list">
-        <li>
+        <li *ngFor="let todo of todos" [class.completed]="todo.status">
           <div class="view">
-            <input class="toggle" type="checkbox">
-            <label>Todo 1</label>
+            <input class="toggle" type="checkbox" [checked]="todo.status" (click)="toggleStatus(todo)">
+            <label>{{todo.name}}</label>
             <button class="destroy"></button>
           </div>
         </li>
-        <li>
-        <div class="view">
-          <input class="toggle" type="checkbox">
-          <label>Todo 2</label>
-          <button class="destroy"></button>
-        </div>
-      </li>
       </ul>
     </section>
   `,
@@ -32,4 +26,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SaarJS Demo';
+  todos = [];
+
+  public addTodo(event) {
+    this.todos.push({
+      name: event.target.value,
+      status: false
+    });
+    event.target.value = '';
+    console.log(this.todos);
+  }
+
+  public toggleStatus(todo) {
+    todo.status = !todo.status;
+    console.log(todo.status);
+  }
 }
